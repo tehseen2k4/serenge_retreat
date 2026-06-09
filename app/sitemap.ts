21c30ce86f37fa829f9bcc505447535c966d2a18
tehseen2k4 <tehseen2k4@gmail.com>
2026-06-09 16:37:47 +0500

    fix primary domain issue in sitemap and robots.txt

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://serengeretreatpakistan.com'
+    const baseUrl = 'https://www.serengeretreatpakistan.com'
     const lastModified = new Date()
 
     return [
